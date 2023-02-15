0ffc6020e2f04419a22e95de23a0d82052c89e4e
Bhavya Hegde <85700971+hegdebhavya@users.noreply.github.com>
2023-02-14 22:33:54 -0800

    Logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -58,4 +58,8 @@ def dashboard(request):
     return render(request,'accounts/dashboard.html')
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'you are successfully logged out.')
+        return redirect('home')
     return redirect('home')
