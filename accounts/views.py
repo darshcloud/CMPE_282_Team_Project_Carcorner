7567ef4830fae39df2db2c722fc01e272b17f0df
Bhavya Hegde <85700971+hegdebhavya@users.noreply.github.com>
2023-02-15 17:51:45 -0800

    Register redirect fix

--- accounts/views.py
+++ accounts/views.py
@@ -42,8 +42,8 @@ def register(request):
                 else:
                     user = User.objects.create_user(first_name=firstname, last_name=lastname, email=email, username=username, password=password)
                     auth.login(request, user)
-                    messages.success(request, 'You are now logged in.')
-                    return redirect('dashboard')
+                    
+                    
                     user.save()
                     messages.success(request, 'You are registered successfully.')
                     return redirect('login')
