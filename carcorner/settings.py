612c4d4821f1ab43176b254498a42b554500316e
Blessy Dickson <dblessy@gmail.com>
2023-02-19 23:24:13 -0800

    add contact details

--- carcorner/settings.py
+++ carcorner/settings.py
@@ -148,3 +148,10 @@ MESSAGE_TAGS = {
 }
 
 SITE_ID = 1;
+
+# Email sending
+EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+EMAIL_PORT = 2525
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
